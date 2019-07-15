3f00a189c9ffec370e6ad7bf52248899dcca7d44
Camille Salikhov <dontlookbackinanger@yandex.ru>
2019-07-15 13:59:03 +0300

    comments fix

--- src/reduxRelated/reduxStore.js
+++ src/reduxRelated/reduxStore.js
@@ -5,7 +5,8 @@ import {rootReducer} from './reducers'
 //create store
 const middleware = [thunk];
 const store = createStore(rootReducer, {}, compose(
-    applyMiddleware(...middleware)
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 ))
 
